f30cc04234fb6cbd86c1ba36a00b46c182a6e38d
Isai Oliva <i.oliva@uniandes.edu.co>
2022-09-21 23:07:51 -0400

    automerge

--- src/__main__.py
+++ src/__main__.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     session.close()
     
     coleccion = Coleccion()
-    ###PLEASE_MERGE
+    ###PLEASE_MERGE.
     app = ic.App(sys.argv, coleccion)
     sys.exit(app.exec_())
