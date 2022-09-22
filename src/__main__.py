a8f2abdd53b7040356bdc61937b3008f7a3069f7
Isai Oliva <i.oliva@uniandes.edu.co>
2022-09-21 22:32:01 -0400

    PLEASE_MERGE carajo

--- src/__main__.py
+++ src/__main__.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     session.close()
     
     coleccion = Coleccion()
-    ###PLEASE_MERGE carajo
+    ###PLEASE_MERGE carajo, carajo
     app = ic.App(sys.argv, coleccion)
     sys.exit(app.exec_())
